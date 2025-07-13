d160ef3da2b9740f4d546ebcc1d4c7a4b7cd05d3
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-14 00:07:41 +0900

    chore: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,49 @@ module.exports = defineConfig([
     ignores: ['dist/*', 'node_modules/*', '.expo/*'],
   },
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       prettier: require('eslint-plugin-prettier'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       'prettier/prettier': 'error',
+
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-console': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-duplicate-imports': 'error',
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
 ]);
