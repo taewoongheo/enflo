48a7c95c6e02d433733c7ef825d38a14de9afb2e
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-25 19:57:03 +0900

    refactor: split SessionCard component

---
+++ components/MainPage/Content/SessionCard.tsx
@@ -0,0 +1,122 @@
+import Typography from '@/components/common/Typography';
+import { useTheme } from '@/contexts/ThemeContext';
+import Session from '@/models/Session';
+import { baseTokens } from '@/styles';
+import { formatMsToTime } from '@/utils/time';
+import { Fontisto } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Platform, StyleSheet, View } from 'react-native';
+import { scale } from 'react-native-size-matters';
+
+const IOS_GRADIENT_OFFSET = 0.15;
+const ANDROID_GRADIENT_OFFSET = 0.1;
+
+function SessionCard({ item }: { item: Session }) {
+  const { theme } = useTheme();
+  const { t } = useTranslation();
+
+  const sessionCardStyle = theme.colors.components.sessionCard;
+  const themeBackground = sessionCardStyle.background;
+  const themeEdgeGradient = sessionCardStyle.edgeGradient;
+
+  const gradientOffset =
+    Platform.OS === 'ios' ? IOS_GRADIENT_OFFSET : ANDROID_GRADIENT_OFFSET;
+
+  return (
+    <View
+      style={[
+        styles.sessionCard,
+        {
+          borderColor: sessionCardStyle.border,
+        },
+      ]}
+    >
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: gradientOffset, y: 0 }}
+        colors={[themeBackground, themeEdgeGradient]}
+        style={styles.linearGradient}
+      />
+
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: gradientOffset }}
+        colors={[themeBackground, themeEdgeGradient]}
+        style={styles.linearGradient}
+      />
+
+      <LinearGradient
+        start={{ x: 1, y: 1 }}
+        end={{ x: 1, y: 1 - gradientOffset }}
+        colors={[themeBackground, themeEdgeGradient]}
+        style={styles.linearGradient}
+      />
+
+      <LinearGradient
+        start={{ x: 1, y: 1 }}
+        end={{ x: 1 - gradientOffset, y: 1 }}
+        colors={[themeBackground, themeEdgeGradient]}
+        style={[styles.linearGradient]}
+      />
+      <Typography
+        variant="body1Regular"
+        style={{ color: sessionCardStyle.text.name }}
+        numberOfLines={2}
+      >
+        {item.sessionName}
+      </Typography>
+      <View>
+        <Typography
+          variant="label"
+          style={{ color: sessionCardStyle.text.label }}
+        >
+          {t('totalDuration')}
+        </Typography>
+        <View style={styles.sessionCardRow}>
+          <Typography
+            variant="title1Bold"
+            style={{
+              color: sessionCardStyle.text.timer,
+            }}
+          >
+            {formatMsToTime(item.totalNetFocusMs)}
+          </Typography>
+          <Fontisto
+            name="play"
+            size={scale(22)}
+            color={sessionCardStyle.text.timer}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default SessionCard;
+
+const styles = StyleSheet.create({
+  sessionCard: {
+    width: scale(200),
+    height: scale(150),
+    padding: baseTokens.spacing[4],
+    borderRadius: baseTokens.borderRadius.lg,
+    justifyContent: 'space-between',
+    borderWidth: 1.3,
+    overflow: 'hidden',
+  },
+  sessionCardRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: baseTokens.spacing[2],
+  },
+  linearGradient: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
--- components/MainPage/Content/SessionList.tsx
+++ components/MainPage/Content/SessionList.tsx
@@ -1,25 +1,13 @@
-import Typography from '@/components/common/Typography';
-import { useTheme } from '@/contexts/ThemeContext';
 import { sessionMockData } from '@/data/sessionMockData';
 import Session from '@/models/Session';
 import { baseTokens } from '@/styles';
-import { formatMsToTime } from '@/utils/time';
-import { Fontisto } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Platform, StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { scale } from 'react-native-size-matters';
+import SessionCard from './SessionCard';
 
 const SessionList = () => {
   const [sessions, setSessions] = useState<Session[]>([]);
-  const { theme } = useTheme();
-  const { t } = useTranslation();
-
-  const themeBackground = theme.colors.components.sessionCard.background;
-  const themeEdgeGradient = theme.colors.components.sessionCard.edgeGradient;
-  const gradientOffset = Platform.OS === 'ios' ? 0.15 : 0.1;
 
   useEffect(() => {
     setSessions(sessionMockData);
@@ -31,98 +19,7 @@ const SessionList = () => {
       horizontal={true}
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={styles.flatListContent}
-      renderItem={({ item }) => (
-        <View
-          style={[
-            styles.sessionCard,
-            {
-              borderColor: theme.colors.components.sessionCard.border,
-            },
-          ]}
-        >
-          <LinearGradient
-            start={{ x: 0, y: 0 }}
-            end={{ x: gradientOffset, y: 0 }}
-            colors={[themeBackground, themeEdgeGradient]}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          />
-
-          <LinearGradient
-            start={{ x: 0, y: 0 }}
-            end={{ x: 0, y: gradientOffset }}
-            colors={[themeBackground, themeEdgeGradient]}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          />
-
-          <LinearGradient
-            start={{ x: 1, y: 1 }}
-            end={{ x: 1, y: 1 - gradientOffset }}
-            colors={[themeBackground, themeEdgeGradient]}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          />
-
-          <LinearGradient
-            start={{ x: 1, y: 1 }}
-            end={{ x: 1 - gradientOffset, y: 1 }}
-            colors={[themeBackground, themeEdgeGradient]}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              borderRadius: baseTokens.borderRadius.lg,
-            }}
-          />
-          <Typography
-            variant="body1Regular"
-            style={{ color: theme.colors.components.sessionCard.text.name }}
-          >
-            {item.sessionName}
-          </Typography>
-          <View>
-            <Typography
-              variant="label"
-              style={{ color: theme.colors.components.sessionCard.text.label }}
-            >
-              {t('totalDuration')}
-            </Typography>
-            <View style={styles.sessionCardRow}>
-              <Typography
-                variant="title1Bold"
-                style={{
-                  color: theme.colors.components.sessionCard.text.timer,
-                }}
-              >
-                {formatMsToTime(item.totalNetFocusMs)}
-              </Typography>
-              <Fontisto
-                name="play"
-                size={scale(22)}
-                color={theme.colors.components.sessionCard.text.timer}
-              />
-            </View>
-          </View>
-        </View>
-      )}
+      renderItem={({ item }) => <SessionCard item={item} />}
     />
   );
 };
@@ -133,20 +30,5 @@ const styles = StyleSheet.create({
   flatListContent: {
     gap: baseTokens.spacing[2],
     marginVertical: baseTokens.spacing[4],
-  },
-  sessionCard: {
-    width: scale(200),
-    height: scale(150),
-    padding: baseTokens.spacing[4],
-    borderRadius: baseTokens.borderRadius.lg,
-    justifyContent: 'space-between',
-    borderWidth: 1.4,
-    overflow: 'hidden',
-  },
-  sessionCardRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    gap: baseTokens.spacing[2],
   },
 });
