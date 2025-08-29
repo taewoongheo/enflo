275e42ee9c0c7f8f4e77ca4d4ac02c3df353e516
Taewoong Heo <chamjoeun0111@gmail.com>
2025-08-29 14:33:54 +0900

    feat: add bottom fade-out effect to particle canvas

--- components/MainPage/EntropyCanvas/EntropyCanvas.tsx
+++ components/MainPage/EntropyCanvas/EntropyCanvas.tsx
@@ -2,7 +2,14 @@ import {
   particleCanvasHeight,
   particleCanvasWidth,
 } from '@/constants/entropySystem/dimension';
-import { Canvas } from '@shopify/react-native-skia';
+import { useEntropyStore } from '@/store/entropyStore';
+import {
+  Canvas,
+  LinearGradient,
+  Mask,
+  Rect,
+  vec,
+} from '@shopify/react-native-skia';
 import React from 'react';
 import {
   Gesture,
@@ -14,7 +21,11 @@ import { useSharedValue } from 'react-native-reanimated';
 import { PanGestureHandlerEventPayload } from 'react-native-screens';
 import EntropySystem from './EntropySystem';
 
+const FADE_START_RATIO = 0.75;
+
 const EntropyCanvas = () => {
+  const entropyScore = useEntropyStore((s) => s.entropyScore);
+
   const touchX = useSharedValue(0);
   const touchY = useSharedValue(0);
   const isTouching = useSharedValue(false);
@@ -54,16 +65,36 @@ const EntropyCanvas = () => {
   return (
     <GestureDetector gesture={combinedGesture}>
       <Canvas
-        style={{
-          width: particleCanvasWidth,
-          height: particleCanvasHeight,
-        }}
+        style={{ width: particleCanvasWidth, height: particleCanvasHeight }}
       >
-        <EntropySystem
-          touchX={touchX}
-          touchY={touchY}
-          isTouching={isTouching}
-        />
+        <Mask
+          mode="alpha"
+          mask={
+            <Rect
+              x={0}
+              y={0}
+              width={particleCanvasWidth}
+              height={particleCanvasHeight}
+            >
+              <LinearGradient
+                start={vec(0, 0)}
+                end={vec(0, particleCanvasHeight)}
+                colors={[
+                  'rgba(255,255,255,1)',
+                  'rgba(255,255,255,1)',
+                  'rgba(255,255,255,0)',
+                ]}
+                positions={[0, FADE_START_RATIO, 1]}
+              />
+            </Rect>
+          }
+        >
+          <EntropySystem
+            touchX={touchX}
+            touchY={touchY}
+            isTouching={isTouching}
+          />
+        </Mask>
       </Canvas>
     </GestureDetector>
   );
--- constants/entropySystem/dimension.ts
+++ constants/entropySystem/dimension.ts
@@ -3,4 +3,4 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export const particleCanvasWidth = width;
-export const particleCanvasHeight = height * 0.6;
+export const particleCanvasHeight = height * 0.65;
