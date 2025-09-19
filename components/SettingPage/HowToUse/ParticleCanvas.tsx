37cf2535cc10dcfc768cac1e32e042c6841dc85e
Taewoong Heo <chamjoeun0111@gmail.com>
2025-09-19 20:41:16 +0900

    feat: add loading state on particle canvas screen

--- components/SettingPage/HowToUse/ParticleCanvas.tsx
+++ components/SettingPage/HowToUse/ParticleCanvas.tsx
@@ -12,7 +12,8 @@ import {
   Rect,
   vec,
 } from '@shopify/react-native-skia';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import {
   Gesture,
   GestureDetector,
@@ -40,6 +41,16 @@ function ParticleCanvas({ low, onTouchableEnable }: ParticleCanvasProps) {
   const isTouching = useSharedValue(false);
 
   const particles = useMemo(() => calculateHowToUseParticles(), []);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loading = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      setIsLoading(false);
+    };
+
+    loading();
+  }, []);
 
   const handleTouch = (
     e:
@@ -79,63 +90,85 @@ function ParticleCanvas({ low, onTouchableEnable }: ParticleCanvasProps) {
   const combinedGesture = Gesture.Race(tap, pan);
 
   return (
-    <GestureDetector gesture={combinedGesture}>
-      <Canvas
-        style={{ width: particleCanvasWidth, height: particleCanvasHeight }}
-      >
-        <Mask
-          mode="alpha"
-          mask={
-            <Group>
-              <Rect
-                x={0}
-                y={0}
-                width={particleCanvasWidth}
-                height={particleCanvasHeight}
-              >
-                <LinearGradient
-                  start={vec(0, 0)}
-                  end={vec(0, particleCanvasHeight)}
-                  colors={[
-                    `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 1)`,
-                    `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 1)`,
-                    `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 0)`,
-                  ]}
-                  positions={[0, FADE_START_RATIO, 1]}
-                />
-              </Rect>
-            </Group>
-          }
+    <View style={{ position: 'relative' }}>
+      {isLoading && (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            backgroundColor: theme.colors.background,
+
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+
+            zIndex: 1000,
+          }}
         >
-          {particles.map((particle, index) => {
-            const radius =
-              Math.random() * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS;
+          <ActivityIndicator size="large" color={theme.colors.text.primary} />
+        </View>
+      )}
+      <GestureDetector gesture={combinedGesture}>
+        <Canvas
+          style={{ width: particleCanvasWidth, height: particleCanvasHeight }}
+        >
+          <Mask
+            mode="alpha"
+            mask={
+              <Group>
+                <Rect
+                  x={0}
+                  y={0}
+                  width={particleCanvasWidth}
+                  height={particleCanvasHeight}
+                >
+                  <LinearGradient
+                    start={vec(0, 0)}
+                    end={vec(0, particleCanvasHeight)}
+                    colors={[
+                      `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 1)`,
+                      `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 1)`,
+                      `rgba(${theme.colors.particles.background}, ${theme.colors.particles.background}, ${theme.colors.particles.background}, 0)`,
+                    ]}
+                    positions={[0, FADE_START_RATIO, 1]}
+                  />
+                </Rect>
+              </Group>
+            }
+          >
+            {particles.map((particle, index) => {
+              const radius =
+                Math.random() * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS;
 
-            const colorValue = theme.colors.particles.base;
-            const maxAlpha = theme.colors.particles.maxAlpha;
-            const minAlpha = theme.colors.particles.minAlpha;
-            const alpha = Math.random() * (maxAlpha - minAlpha) + minAlpha;
-            const color = `rgba(${colorValue}, ${colorValue}, ${colorValue}, ${alpha})`;
+              const colorValue = theme.colors.particles.base;
+              const maxAlpha = theme.colors.particles.maxAlpha;
+              const minAlpha = theme.colors.particles.minAlpha;
+              const alpha = Math.random() * (maxAlpha - minAlpha) + minAlpha;
+              const color = `rgba(${colorValue}, ${colorValue}, ${colorValue}, ${alpha})`;
 
-            return (
-              <DynamicParticle
-                key={`particle-${index}-${particle.x}-${particle.y}`}
-                centerX={particle.x}
-                centerY={particle.y}
-                nextX={particle.nextX}
-                nextY={particle.nextY}
-                radius={radius}
-                color={color}
-                touchX={touchX}
-                touchY={touchY}
-                isTouching={isTouching}
-                low={low}
-              />
-            );
-          })}
-        </Mask>
-      </Canvas>
-    </GestureDetector>
+              return (
+                <DynamicParticle
+                  key={`particle-${index}-${particle.x}-${particle.y}`}
+                  centerX={particle.x}
+                  centerY={particle.y}
+                  nextX={particle.nextX}
+                  nextY={particle.nextY}
+                  radius={radius}
+                  color={color}
+                  touchX={touchX}
+                  touchY={touchY}
+                  isTouching={isTouching}
+                  low={low}
+                />
+              );
+            })}
+          </Mask>
+        </Canvas>
+      </GestureDetector>
+    </View>
   );
 }
 
