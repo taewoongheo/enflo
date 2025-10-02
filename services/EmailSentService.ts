9d00de074d2f40033b9f6c6fa7931ce3c52695ed
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-02 14:24:53 +0900

    feat(service): add EmailSentService for managing email sending

---
+++ services/EmailSentService.ts
@@ -0,0 +1,63 @@
+import { db } from '@/db/db';
+import { emailLastSent } from '@/db/schema';
+import { signRequest } from '@/utils/auth';
+import { eq, sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import Constants from 'expo-constants';
+
+const EMAIL_API_URL = Constants.expoConfig?.extra?.EMAIL_API_URL;
+
+class EmailSentService {
+  private db: ReturnType<typeof drizzle>;
+
+  constructor(db: ReturnType<typeof drizzle>) {
+    this.db = db;
+  }
+
+  async sendEmail(
+    type: 'feedback' | 'promotion',
+    timestamp: string,
+    rawBody: string,
+  ) {
+    try {
+      const row = await this.db
+        .select()
+        .from(emailLastSent)
+        .where(eq(emailLastSent.emailType, type));
+
+      const lastSent = row[0]?.sentAt?.getTime();
+
+      const now = new Date().getTime();
+      if (lastSent && now - lastSent < 1000 * 60 * 60 * 24) {
+        return;
+      }
+
+      await fetch(`${EMAIL_API_URL}/feedback`, {
+        method: 'POST',
+        body: rawBody,
+        headers: {
+          'Content-Type': 'application/json',
+          'x-timestamp': timestamp,
+          'x-signature': signRequest('POST', '/feedback', timestamp, rawBody),
+        },
+      });
+
+      await this.db
+        .insert(emailLastSent)
+        .values({
+          emailType: type,
+          sentAt: sql`(strftime('%s','now')*1000)`,
+        })
+        .onConflictDoUpdate({
+          target: emailLastSent.emailType,
+          set: {
+            sentAt: sql`(strftime('%s','now')*1000)`,
+          },
+        });
+    } catch (error) {
+      throw new Error('Failed to send email', { cause: error });
+    }
+  }
+}
+
+export default new EmailSentService(db);
